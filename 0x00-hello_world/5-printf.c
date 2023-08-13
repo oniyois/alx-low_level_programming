2034147147f6588ca07f3150a96cd01ee3287ea7
oniyois <oniyoislamiyat@gmail.com>
2023-08-13 08:55:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is function tha prints with printf
+ *
+ * Return: 0 when succesfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
+
