f1f200cf48f522c7d247bb1358f55d0afbee19da
oniyois <oniyoislamiyat@gmail.com>
2023-08-15 15:14:45 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,14 +6,12 @@
  * @a: int to be added to b
  * @b: int to be added to a
  * Return: sum of a and b
- */	
+ */
 int add(int a, int b)
 {
 	int c;
 
+	c = a + b;
 
-                c = a + b;
-
-
-                return (c);
+	return (c);
 }
