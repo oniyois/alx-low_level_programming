6ca7d9f51f3fd3654f28a42607ecefa2da8089b8
oniyois <oniyoislamiyat@gmail.com>
2023-08-15 14:15:36 -0700

    5

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,21 +8,20 @@
 void print_alphabet_x10(void)
 {
 	char ch;
-                int i;
+	 int i;
 
+	 i = 0;
 
-                i = 0;
+	 while (i < 10)
+	 {
+		  ch = 'a';
+		  while (ch <= 'z')
+		  {
 
-
-                while (i < 10)
-                {
-                        ch = 'a';
-                        while (ch <= 'z')
-                        {
-                                _putchar(ch);
-                                ch++;
-                        }
-                        _putchar('\n');
-                        i++;
-                }
+			  _putchar(ch);
+			   ch++;
+		  }
+		   _putchar('\n');
+		   i++;
+	 }
 }
